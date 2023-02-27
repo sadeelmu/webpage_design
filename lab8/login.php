8c815fffef86256031bab3c3354af3af78c377d0
Sadel Muwahed <68566188+sadeelmu@users.noreply.github.com>
2023-02-27 06:50:57 +0300

    Create login.php

---
+++ lab8/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="handle_login.php" method="post">
+
+    <br>
+    <label for="emailText">emailText </label>
+    <input type="email" name="emailText" id="emailText">
+    <br>
+
+    <label for="pass">Password </label>
+    <input type="password" name="pass" id="pass">
+    <br>
+
+    <input type="submit" value="submit">
+</form>
+</body>
+</html>
